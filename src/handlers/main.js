3bcdc785eeae7ebb78fb59fd484f047b043e2322
ArchitGajjar <architgajjar1992@gmail.com>
2018-08-26 21:56:05 -0700

    Updating main.js with promise.all

--- src/handlers/main.js
+++ src/handlers/main.js
@@ -2,27 +2,26 @@ var popGen = require('../lib/popGen'),
   dataStore = require('../lib/dataStore');
 
 module.exports = (request, reply) => {
-  var context = {};
+  const promises = [
+    popGen.getPopularSlices(),
+    popGen.getMostPopular(),
+    popGen.getNewestSlice(),
+    popGen.getMostImproved(),
+    dataStore.getPizzas()
+  ]
 
-  popGen.getPopularSlices((err, popSlices) => {
-    context.popSlices = popSlices;
+  Promise.all(promises)
+  .then((results) => {
+    const context = {};
+    context.popSlices = results[0];
+    context.mostPopular = results[1];
+    context.newestSlice = results[2];
+    context.mostImproved = results[3];
+    context.pizzas = results[4];
 
-    popGen.getMostPopular((err, mostPopular) => {
-      context.mostPopular = mostPopular;
-
-      popGen.getNewestSlice((err, newestSlice) => {
-        context.newestSlice = newestSlice;
-
-        popGen.getMostImproved((err, mostImproved) => {
-          context.mostImproved = mostImproved;
-
-          dataStore.getPizzas((err, pizzas) => {
-            context.pizzas = pizzas;
-
-            return reply.view('index', context);
-          });
-        });
-      });
-    });
+    return reply.view('index', context);
+  })
+  .catch((err)=> {
+    console.error(`ERROR : ${JSON.stringify(err)}`);
   });
 };
